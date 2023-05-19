d8c0eb8fed40210cf8ae559da1f459c7a6a25d96
Henry Till <henrytill@gmail.com>
2023-05-19 12:23:33 -0700

    threadtest: tweak

--- threadtest.c
+++ threadtest.c
@@ -9,11 +9,11 @@
 #include <string.h>
 #include <unistd.h>
 
-struct ThreadInfo { /* Used as argument to thread_start() */
-  pthread_t tid;    /* ID returned by pthread_create() */
-  int tnum;         /* Application-defined thread # */
-  char *arg;        /* From command-line argument */
-};
+typedef struct thread_info { /* Used as argument to thread_start() */
+  pthread_t tid;             /* ID returned by pthread_create() */
+  int tnum;                  /* Application-defined thread # */
+  char *arg;                 /* From command-line argument */
+} thread_info;
 
 static inline void handle_errno(int err, const char *msg) {
   errno = err;
@@ -29,15 +29,16 @@ static inline void handle_error(const char *msg) {
 /* Thread start function: display address near top of our stack,
    and return upper-cased copy of argv_string. */
 static void *start(void *arg) {
-  struct ThreadInfo *tinfo = arg;
-  char *ret;
-
-  printf("Thread %d: top of stack near %p; argv_string=%s\n", tinfo->tnum, (void *)&tinfo, tinfo->arg);
-  ret = strdup(tinfo->arg);
-  if (ret == NULL)
+  thread_info *tinfo = arg;
+  printf("Thread %d: top of stack near %p; argv_string=%s\n",
+         tinfo->tnum, (void *)&tinfo, tinfo->arg);
+  char *ret = strdup(tinfo->arg);
+  if (ret == NULL) {
     handle_error("strdup");
-  for (char *p = ret; *p != '\0'; ++p)
+  }
+  for (char *p = ret; *p != '\0'; ++p) {
     *p = (char)toupper(*p);
+  }
   return ret;
 }
 
@@ -64,19 +65,22 @@ int main(int argc, char *argv[]) {
 
   /* Initialize thread creation attributes. */
   rc = pthread_attr_init(&attr);
-  if (rc != 0)
+  if (rc != 0) {
     handle_errno(rc, "pthread_attr_init");
+  }
 
   if (stacksz > 0) {
     rc = pthread_attr_setstacksize(&attr, stacksz);
-    if (rc != 0)
+    if (rc != 0) {
       handle_errno(rc, "pthread_attr_setstacksize");
+    }
   }
 
   /* Allocate memory for pthread_create() arguments. */
-  struct ThreadInfo *tinfo = calloc((size_t)nthreads, sizeof(*tinfo));
-  if (tinfo == NULL)
+  thread_info *tinfo = calloc((size_t)nthreads, sizeof(*tinfo));
+  if (tinfo == NULL) {
     handle_error("calloc");
+  }
 
   /* Create one thread for each command-line argument. */
   for (int i = 0; i < nthreads; ++i) {
@@ -85,21 +89,23 @@ int main(int argc, char *argv[]) {
 
     /* The pthread_create() call stores the thread ID into corresponding element of tinfo[]. */
     rc = pthread_create(&tinfo[i].tid, &attr, &start, &tinfo[i]);
-    if (rc != 0)
+    if (rc != 0) {
       handle_errno(rc, "pthread_create");
+    }
   }
 
   /* Destroy the thread attributes object, since it is no longer needed. */
   rc = pthread_attr_destroy(&attr);
-  if (rc != 0)
+  if (rc != 0) {
     handle_errno(rc, "pthread_attr_destroy");
+  }
 
   /* Now join with each thread, and display its returned value. */
   for (int i = 0; i < nthreads; ++i) {
     rc = pthread_join(tinfo[i].tid, &res);
-    if (rc != 0)
+    if (rc != 0) {
       handle_errno(rc, "pthread_join");
-
+    }
     printf("Joined with thread %d; returned value was %s\n", tinfo[i].tnum, (char *)res);
     free(res); /* Free memory allocated by thread */
   }
