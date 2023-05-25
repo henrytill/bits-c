969eac69af1006221bc53f89fe3f517b528bd623
Henry Till <henrytill@gmail.com>
2023-05-24 18:36:23 -0700

    base64: add length test, improve cleanup

--- base64.c
+++ base64.c
@@ -26,41 +26,47 @@ static long base64_decode(const unsigned char *in, int in_len,
 
 int main(void)
 {
+    int ret = EXIT_FAILURE;
     long codec_len = 0L;
     const char *const input = "foobar";
     const size_t input_len = strlen(input);
     const size_t input_base64_len = base64_strlen(input);
 
     printf("input: %s\n", input);
-    printf("strlen(input): %ld\n", input_len);
-    printf("base64_len: %ld\n\n", input_base64_len);
 
     char *base64 = emalloc(input_base64_len + 1);
     codec_len = base64_encode((const unsigned char *)input,
                               (int)input_len,
                               (unsigned char *)base64);
 
-    printf("len: %ld\n", codec_len);
-    printf("strlen(base64): %ld\n", strlen(base64));
-    printf("base64: %s\n\n", base64);
+    printf("base64: %s\n", base64);
+
+    if (strlen(base64) != input_base64_len) {
+        (void)fprintf(stderr, "strlen(base64) != input_base_64_len");
+        goto out_free_base64;
+    }
 
     char *output = emalloc(input_len + 1);
     codec_len = base64_decode((const unsigned char *)base64,
                               (int)codec_len,
                               (unsigned char *)output);
 
-    printf("len: %ld\n", codec_len);
-    printf("strlen(output): %ld\n", strlen(output));
     printf("output: %s\n", output);
+
+    if (strlen(output) != (size_t)codec_len) {
+        (void)fprintf(stderr, "strlen(output) != codec_len");
+        goto out_free_output;
+    }
 
     if (strcmp(input, output) != 0) {
         (void)fprintf(stderr, "input != output\n");
-        free(output);
-        free(base64);
-        return EXIT_FAILURE;
+        goto out_free_output;
     }
 
+    ret = EXIT_SUCCESS;
+out_free_output:
     free(output);
+out_free_base64:
     free(base64);
-    return EXIT_SUCCESS;
+    return ret;
 }
