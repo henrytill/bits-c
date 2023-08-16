88beee33140ba846f27915c9743c8622f231fa0d
Henry Till <henrytill@gmail.com>
2023-08-16 01:39:37 -0700

    base64: remove dynamic allocation

--- base64.c
+++ base64.c
@@ -5,16 +5,14 @@
 
 #include <openssl/evp.h>
 
-#include "alloc.h"
-
 #define STATIC_ASSERT(e) _Static_assert((e), #e)
 
 #define BASE64_STRLEN(s) (((strlen((s)) + 2) / 3) * 4)
 
-#define TEST(e, l)                                              \
+#define TEST(e)                                                 \
   if (!(e)) {                                                   \
     (void)fprintf(stderr, "%s:%d: %s", __FILE__, __LINE__, #e); \
-    goto l;                                                     \
+    exit(EXIT_FAILURE);                                         \
   }
 
 // https://boringssl.googlesource.com/boringssl/+/master/crypto/base64/base64_test.cc#49
@@ -43,13 +41,19 @@ static const struct test_vector {
   {NULL, 0, NULL, 0},
 };
 
-static size_t base64_encode(const size_t in_len, const char in[in_len + 1], char *out) {
+#define ARRAY_SIZE(arr)  (sizeof((arr)) / sizeof((arr)[0]))
+#define LAST_ENTRY       (ARRAY_SIZE(TEST_VECTORS) - 2)
+#define INPUT_LEN_MAX    (TEST_VECTORS[LAST_ENTRY].input_len)
+#define BASE64_LEN_MAX   (TEST_VECTORS[LAST_ENTRY].base64_len)
+#define ARRAY_CLEAR(arr) memset((arr), 0, sizeof((arr)))
+
+static size_t base64_encode(const size_t in_len, const char in[in_len], char *out) {
   return (size_t)EVP_EncodeBlock((unsigned char *)out,
                                  (const unsigned char *)in,
                                  (int)in_len);
 }
 
-static size_t base64_decode(const size_t in_len, const char in[in_len + 1], char *out) {
+static size_t base64_decode(const size_t in_len, const char in[in_len], char *out) {
   return (size_t)EVP_DecodeBlock((unsigned char *)out,
                                  (const unsigned char *)in,
                                  (int)in_len);
@@ -65,12 +69,13 @@ int main(void) {
 
   size_t codec_len = 0;
 
-  char *actual_base64 = NULL;
-  size_t actual_base64_len = 0;
-  char *output = NULL;
-  size_t output_len = 0;
+  char actual_base64[BASE64_LEN_MAX + 1];
+  char output[INPUT_LEN_MAX + 1];
 
   for (size_t i = 0; (input = TEST_VECTORS[i].input) != NULL; ++i) {
+    ARRAY_CLEAR(actual_base64);
+    ARRAY_CLEAR(output);
+
     input_len = TEST_VECTORS[i].input_len;
     expected_base64 = TEST_VECTORS[i].base64;
     expected_base64_len = TEST_VECTORS[i].base64_len;
@@ -80,36 +85,20 @@ int main(void) {
     printf("expected_base64: %s\n", expected_base64);
     printf("expected_base64_len: %ld\n", expected_base64_len);
 
-    actual_base64 = ecalloc(expected_base64_len + 1, sizeof(*actual_base64));
     codec_len = base64_encode(input_len, input, actual_base64);
-    actual_base64_len = strlen(actual_base64);
 
-    TEST(codec_len == actual_base64_len, out_free_actual_base64);
+    TEST(codec_len == strlen(actual_base64));
+    TEST(codec_len == expected_base64_len);
+    TEST(strcmp(actual_base64, expected_base64) == 0);
 
-    TEST(actual_base64_len == expected_base64_len, out_free_actual_base64);
-
-    TEST(strcmp(actual_base64, expected_base64) == 0, out_free_actual_base64);
-
-    output = ecalloc(input_len + 1, sizeof(*output));
     codec_len = base64_decode(codec_len, actual_base64, output);
-    output_len = strlen(output);
 
     printf("codec_len: %ld\n", codec_len);
     printf("\n");
 
-    TEST(output_len == input_len, out_free_output);
-
-    TEST(strcmp(output, input) == 0, out_free_output);
-
-    NFREE(output);
-    NFREE(actual_base64);
+    TEST(input_len == strlen(output));
+    TEST(strcmp(output, input) == 0);
   }
 
   return EXIT_SUCCESS;
-
-out_free_output:
-  NFREE(output);
-out_free_actual_base64:
-  NFREE(actual_base64);
-  return EXIT_FAILURE;
 }
