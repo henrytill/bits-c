ed115f4bdaeb9de7b9560bb0b36b7e1d3d4d8cc9
Henry Till <henrytill@gmail.com>
2023-08-28 16:36:57 -0700

    add hypothesis-based hashtable test

--- hashtable_test.py
+++ hashtable_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from hypothesis import given, strategies as st
+
 import hashtable
 
 
@@ -30,6 +32,29 @@ class TestHashtable(unittest.TestCase):
         hashtable.put(self.table, "key1", "new_value1")
         value = hashtable.get(self.table, "key1")
         self.assertEqual(value, "new_value1")
+
+
+class TestHashtableWithHypothesis(unittest.TestCase):
+    @given(st.dictionaries(st.text().filter(lambda x: '\x00' not in x), st.text()))
+    def test_put_get_with_random_data(self, input_dict):
+        table = hashtable.create(16)
+
+        # Insert all key-value pairs from the input_dict into the hashtable
+        for key, value in input_dict.items():
+            hashtable.put(table, key, value)
+
+        # Check that all inserted key-value pairs can be retrieved
+        for key, value in input_dict.items():
+            retrieved_value = hashtable.get(table, key)
+            self.assertEqual(retrieved_value, value)
+
+        hashtable.destroy(table)
+
+    @given(st.text().filter(lambda x: '\x00' not in x))
+    def test_non_existent_key_returns_none(self, key):
+        table = hashtable.create(16)
+        self.assertIsNone(hashtable.get(table, key))
+        hashtable.destroy(table)
 
 
 if __name__ == "__main__":
