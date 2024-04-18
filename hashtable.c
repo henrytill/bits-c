5284b2fcf22647ffef468875fff449f5385d6ca1
Henry Till <henrytill@gmail.com>
2024-04-18 02:09:14 -0700

    hashtable: move ISPOW2 definition and tests

--- hashtable.c
+++ hashtable.c
@@ -8,6 +8,40 @@
 #include "fnv.h"
 #include "macro.h"
 
+/// Returns 1 if x is a power of 2
+#define ISPOW2(x) (((x) & ((x)-1)) == 0)
+
+#define STATIC_ASSERT(e) _Static_assert((e), #e)
+
+#define TEST_ENTRIES \
+    X(1, 0)          \
+    X(1, 1)          \
+    X(1, 2)          \
+    X(0, 3)          \
+    X(1, 4)          \
+    X(0, 5)          \
+    X(0, 6)          \
+    X(0, 7)          \
+    X(1, 8)          \
+    X(0, 9)          \
+    X(0, 15)         \
+    X(1, 16)         \
+    X(0, 17)         \
+    X(0, 31)         \
+    X(1, 32)         \
+    X(0, 33)         \
+    X(0, 63)         \
+    X(1, 64)         \
+    X(0, 65)         \
+    X(1, 128)
+
+#define X(expected, x) STATIC_ASSERT((expected) == ISPOW2((x)));
+TEST_ENTRIES
+#undef X
+
+#undef TEST_ENTRIES
+#undef STATIC_ASSERT
+
 struct entry {
     struct entry *next;
     const char *key;
