b117403efea64529cbb36fd38ee3a505a418e4b7
Henry Till <henrytill@gmail.com>
2024-09-29 15:20:36 -0700

    lib/message_queue: consolidate includes

--- lib/message_queue.c
+++ lib/message_queue.c
@@ -1,11 +1,10 @@
 #include "message_queue.h"
 
 #include <errno.h>
-#include <stdint.h>
-#include <stdlib.h>
-
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 struct message_queue {
   struct message *buffer; // Buffer to hold messages
