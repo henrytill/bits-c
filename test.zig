b39b6928370f2402e3ca07bb41a961760a2332f8
Henry Till <henrytill@gmail.com>
2024-11-15 01:13:07 -0800

    test.zig: factor out roundtrip

--- test.zig
+++ test.zig
@@ -2,19 +2,20 @@ const c = @cImport(@cInclude("hashtable.h"));
 const std = @import("std");
 const testing = std.testing;
 
+fn roundtrip(comptime T: type, t: *c.struct_table, key: [*c]const u8, value: T) !T {
+    const rc = c.table_put(t, key, @as(*anyopaque, @constCast(value)));
+    try testing.expectEqual(rc, 0);
+    const retPtr = c.table_get(t, key) orelse return error.Failure;
+    return @ptrCast(@alignCast(retPtr));
+}
+
 test "roundtrip i32" {
     const t = c.table_create(8) orelse return error.Failure;
     defer c.table_destroy(t, null);
 
     const key = "key";
     const expected: i32 = 42;
-    const expected_ptr = @as(*anyopaque, @constCast(&expected));
-
-    const rc = c.table_put(t, key, expected_ptr);
-    try testing.expectEqual(rc, 0);
-
-    const valPtr: *anyopaque = c.table_get(t, key) orelse return error.Failure;
-    const actual: @TypeOf(&expected) = @ptrCast(@alignCast(valPtr));
+    const actual = try roundtrip(@TypeOf(&expected), t, key, &expected);
 
     try testing.expectEqual(&expected, actual);
     try testing.expectEqual(expected, actual.*);
@@ -26,13 +27,7 @@ test "roundtrip string" {
 
     const key = "key";
     const expected = "value";
-    const expectedPtr = @as(*anyopaque, @constCast(expected.ptr));
-
-    const rc = c.table_put(t, key, expectedPtr);
-    try testing.expectEqual(rc, 0);
-
-    const valPtr: *anyopaque = c.table_get(t, key) orelse return error.Failure;
-    const actual: @TypeOf(expected) = @ptrCast(@alignCast(valPtr));
+    const actual = try roundtrip(@TypeOf(expected), t, key, expected);
 
     try testing.expectEqual(expected.ptr, actual);
     try testing.expectEqualStrings(expected, actual);
